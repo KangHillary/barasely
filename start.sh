855abd252ff1f0321a7f1359f8ea81be36cc7f25
hchepsergon <h.chepsergon@pegb.tech>
2020-08-08 00:54:38 +0300

    added django jenkins

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting the tests ..."
-docker-compose run web python manage.py test
+docker-compose run web python manage.py jenkins
 echo "finisjed steop one the tests ..."
-python manage.py jenkins
+#python manage.py jenkins
 echo "completed tests..."
 exec "$@"
